a6a541f418f10d8bf7f36a50be959c39db3148b5
TrBoop <78387622+TrBoop@users.noreply.github.com>
2022-12-11 18:36:54 +0000

    Imported Bootstrap, Router and frontend components

---
+++ mainApp/frontend/src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row h-100 justify-content-center align-items-center">
+        <div class="col-10 col-md-8 col-lg-4">
+            <div class="border rounded bg-light shadow p-5 ">
+                <h1 class="h3 mb-4">Sign In</h1>
+                <input type="email" id="inputEmail" class="form-control" placeholder="Email address"/>
+                <br/>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required />
+                <br/>
+                <div class="d-grid gap-2 col-6 mx-auto">
+                    <button class="btn btn-primary" type="submit">Sign In</button>
+                </div>
+                
+                <!-- The a href needs to be replaced by a router -->
+                <p><br/>Not a member? <a href="#!">Register</a></p> 
+            </div>
+        </div>
+    </div>
+</template>
