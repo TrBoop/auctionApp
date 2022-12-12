321c6da36bc1b14014297d33bf0fcfc74086ffde
Tom Hrdlicka <tomhr@email.cz>
2022-12-12 11:51:25 +0000

    created first migration, superuser

---
+++ auctionApp/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from datetime import date
+import random
+from django.contrib.auth.models import UserManager
+
+class CustomUser(AbstractUser):
+
+#resource used to construct model: https://docs.djangoproject.com/en/4.1/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
+
+    userPassword = models.CharField(max_length=40)
+    username = models.CharField(max_length=40, unique = True, primary_key=True)
+    userEmail = models.EmailField(max_length = 254, unique = True)
+    userDateOfBirth = models.DateField(max_length=8)
+
+    #images in a model: https://stackoverflow.com/questions/6396442/add-image-avatar-field-to-users
+    userImage = models.ImageField(upload_to='images')
+
+    USERNAME_FIELD = 'userEmail'
+    REQUIRED_FIELDS = ['username', 'userDateOfBirth']
+
+    objects = UserManager()
+
+
+    """ Returns object name """
+    def __str__(self):
+        return self.userName
+
+    """ Converts the variables to dictionary """
+    def to_dict(self):
+        return {
+            'id' : self.id,
+            'username': self.username,
+            'password': self.userPassword,
+            'email' : self.userEmail,
+            'image' :self.userImage,
+            'DateOfBirth' : self.userDateOfBirth,
+
+
+        }
+    #orders db entries by email address and names the table User
+    class Meta:
+        ordering = ["userEmail"]
+        verbose_name = "User"
--- auctionApp/settings.py
+++ auctionApp/settings.py
@@ -37,14 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    "corsheaders",
+    "auctionApp",
     'mainApp',
 ]
+
+AUTH_USER_MODEL = 'auctionApp.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
