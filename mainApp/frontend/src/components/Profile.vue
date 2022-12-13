357589db3903c274a4c8ee78868e5c9aca2df2a9
TrBoop <78387622+TrBoop@users.noreply.github.com>
2022-12-13 14:05:42 +0000

    Edit Profile Page

--- mainApp/frontend/src/components/Profile.vue
+++ mainApp/frontend/src/components/Profile.vue
@@ -1,5 +1,30 @@
 <template>
     <div class="profile">
-    <h1> Profile </h1>
+        <br/>
+    
+    <div class="row justify-content-center align-items-center" style="min-width: 320px">
+        <div class="col-5">
+            <br/>
+            <div class="border rounded bg-light p-5 ">
+                <h1 class="h3 mb-4">Edit Profile</h1>
+                <br/>
+                <input type="email" id="inputEmail" class="form-control" placeholder="Email address"/>
+                <br/>
+                <input type="text" id="inputUser" class="form-control" placeholder="Username"/>
+                <br/>
+                <input type="date" id="birthday" class="form-control" name="birthday"/>
+                <br/>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required />
+                <br/>
+                <input type="password" id="confirmPassword" class="form-control" placeholder="Confirm Password" required />
+                <br/>
+                <div class="d-grid gap-2 col-6 mx-auto">
+                    <button class="btn btn-primary" type="submit">Save Changes</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 </div>
 </template>
