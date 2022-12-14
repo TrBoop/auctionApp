d244bcdd1a93948d6435ceb52bb62b9d5840230c
TrBoop <78387622+TrBoop@users.noreply.github.com>
2022-12-14 16:59:26 +0000

    Added Auction and Bid Tables

--- auctionApp/admin.py
+++ auctionApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
-
+from .models import Auction
 
 admin.site.register(CustomUser)
+admin.site.register(Auction)
