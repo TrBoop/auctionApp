575d4dd09d1536cf245e2ceb0d27e6235a8761cb
TrBoop <78387622+TrBoop@users.noreply.github.com>
2022-12-14 17:09:34 +0000

    Created Auction listing and Bid tables

--- auctionApp/admin.py
+++ auctionApp/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from .models import Auction
+from .models import Bid
 
 
 admin.site.register(CustomUser)
+admin.site.register(Auction)
+admin.site.register(Bid)
