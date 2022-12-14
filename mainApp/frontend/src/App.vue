8c960b968a5f692a510d9a2419bbf5382e13bcae
Tom Hrdlicka <tomhr@email.cz>
2022-12-14 18:08:25 +0000

    added fetch functions for user

--- mainApp/frontend/src/App.vue
+++ mainApp/frontend/src/App.vue
@@ -1,8 +1,78 @@
 <script>
-import Profile from './components/Profile.vue'
-import Login from './components/Login.vue'
-import Home from './components/Home.vue'
-import Navbar from './components/Navbar.vue'
+  export default{
+    name: 'App',
+    data(){
+      return{
+        users: [],
+        user:{
+          'userPassword': '',
+          'username': '',
+          'userEmail': '',
+          'userDateOfBirth': '',
+          'userImage': '',
+        }
+      } 
+    },
+
+    async created(){
+       await this.getUsers();
+      },
+    methods:{
+
+      submitUser(){
+        if(this.record.id ===undefined){
+          this.addUser();
+        }
+        else{
+          this.updateUser();
+        }
+      },
+
+      async getUsers(){
+        var response = await fetch("http://127.0.0.1:8000/api/records/")
+        let data = await response.json()
+        this.records = data 
+      },
+
+      async addUser(){
+        await this.getUsers();
+        await fetch("http://127.0.0.1:8000/api/records/",{
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(this.user)  
+        });
+        await this.getUsers();
+        this.user = {};
+      },
+
+      async updateUser(){
+        await this.getUsers();
+        await fetch(`http://127.0.0.1:8000/api/records/${this.user.id}/`,{
+          method: 'put',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(this.record)  
+        });
+        await this.getUSers();
+        this.record = {};
+      },
+
+      async deleteUser(user){
+        await this.getRecords();
+        await fetch(`http://127.0.0.1:8000/api/records/${user.id}/`,{
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(this.user)  
+        });
+        await this.getUsers();
+      }
+    }
+  }
 </script>
 
 <template>
