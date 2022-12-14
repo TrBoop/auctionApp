171c4c696fda36d574eb4a4e10a22c6e306031c4
Tom Hrdlicka <tomhr@email.cz>
2022-12-14 17:09:10 +0000

    fixed serializer

--- auctionApp/urls.py
+++ auctionApp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework import routers
-from .views import RecordsViewSet
+from .views import UserViewSet
 #routes the data to be viewed from the db
 router = routers.DefaultRouter()
-router.register('users', RecordsViewSet)
+router.register('users', UserViewSet)
 urlpatterns = router.urls
