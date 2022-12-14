2e58bbdf36a9647cef0d27fbc2884e89ce216cc6
TrBoop <78387622+TrBoop@users.noreply.github.com>
2022-12-14 16:31:16 +0000

    Revert "Reimplement Vues and Router"
    This reverts commit 1ba646dbb950817497aab14b1a3b302a08517b1b.

--- mainApp/frontend/src/App.vue
+++ mainApp/frontend/src/App.vue
@@ -1,15 +1,17 @@
-<script setup lang="ts">
-import Navbar from './components/Navbar.vue'
-import Home from './components/Home.vue'
-import Profile from './components/Profile.vue'
-import Login from './components/Login.vue'
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-<Navbar/>
-
-<router-view :key="$route.path" />
-
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
--- mainApp/frontend/src/main.js
+++ mainApp/frontend/src/main.js
@@ -2,12 +2,4 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-import router from './router'
-
-
-
-
-createApp(App).use(router).mount('#app')
+createApp(App).mount('#app')
