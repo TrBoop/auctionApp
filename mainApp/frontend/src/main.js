728e5cc15778580d8bf51ca6dc6cf8371bc3d04d
Lufdee <dinosc114@hotmail.co.uk>
2022-12-15 21:20:36 +0000

    'registration'

--- mainApp/frontend/src/main.js
+++ mainApp/frontend/src/main.js
@@ -1,11 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+// import { ErrorService } from "./Services/ErrorService";
+// import Vue from "vue";
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-import router from './router'
+import router from './router';
+
+
+// Vue.config.productionTip = false;
+// Vue.config.errorHandler = (error) => ErrorService.onError(error);
 
 createApp(App).use(router).mount('#app')
 
