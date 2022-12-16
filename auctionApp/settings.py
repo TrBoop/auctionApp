f3b226fdd8d96c5acb91502153cf1c4e0b777a6f
Tom Hrdlicka <tomhr@email.cz>
2022-12-16 13:10:09 +0000

    routing

--- auctionApp/settings.py
+++ auctionApp/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    
 ]
 
 CSRF_TRUSTED_ORIGINS = ['http://localhost:5173']
