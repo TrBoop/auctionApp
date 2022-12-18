b00e5321d868e5e17dd4bd3372306b5469e13a96
TrBoop <78387622+TrBoop@users.noreply.github.com>
2022-12-18 21:44:19 +0000

    Search Bar and Removing Outdated Auctions

--- mainApp/frontend/src/components/Home.vue
+++ mainApp/frontend/src/components/Home.vue
@@ -8,7 +8,11 @@
 </div>
 <br/>
 <div class="row">
-
+  <div class="input-group" style="margin-bottom:1rem;">
+    <input type="search" class="form-control rounded" placeholder="Search" />
+    <button type="button" class="btn btn-outline-primary">Search</button>
+  </div>
+  
 <table class="table border rounded bg-light shadow">
         <thead>
           <tr>
@@ -19,17 +23,17 @@
           </tr>
         </thead>
         <tbody class="align-middle" v-for="auction in auctions" :key="auction.itemFinishDate" @dblclick="$data.auction = auction">
-          <tr style="background:#E1E1E1;">
+          <tr style="background:#E1E1E1;" >
               <td v-if="skippableDate(auction.itemFinishDate)">{{auction.itemTitle}} </td>
-              <td v-if="skippableDate(auction.itemFinishDate)"><img src="{{auction.itemPicture}}"/></td>
+              <td v-if="skippableDate(auction.itemFinishDate)"> <img src="{{auction.itemPicture}}" style="width:500px;"/></td>
               <td v-if="skippableDate(auction.itemFinishDate)">{{auction.itemStartPrice}} </td>
               <td v-if="skippableDate(auction.itemFinishDate)">{{auction.itemFinishDate}}</td>
               <td v-if="skippableDate(auction.itemFinishDate)"></td>
-              <td>
-                <tr><input type=number min="0"/></tr>
+              <td v-if="skippableDate(auction.itemFinishDate)">
+                <tr ><input type=number min="0"/></tr>
                 <tr><button type="button" class="btn btn-primary">Bid Amount</button></tr>
               </td>
-              <td>
+              <td v-if="skippableDate(auction.itemFinishDate)">
                 <tr><textarea/></tr>
                 <tr><button type="button" class="btn btn-primary">Submit Question</button></tr>
               </td>
@@ -37,11 +41,11 @@
             </tr>
           
           
-          <tr> 
+          <tr v-if="skippableDate(auction.itemFinishDate)"> 
             <th></th> <!-- Leave Empty -->
             <th scope="col">Questions</th>
           </tr>
-          <tr>
+          <tr v-if="skippableDate(auction.itemFinishDate)">
             <th></th> <!-- Leave Empty -->
             <th scope="col">User</th>
             <th scope="col">Question</th>
@@ -56,6 +60,7 @@
               <th></th> <!-- Leave Empty -->
               <th></th> <!-- Leave Empty -->
               <th scope="col">Answer</th>
+              
             </tr>
 
             <tr>
